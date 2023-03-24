efff31e4624d1d147029011e05ea771a5fe67de0
Nick-Mc <njm24@iastate.edu>
2023-03-24 01:17:13 -0500

    Fri Mar 24 01:17:11 CDT 2023

---
+++ ghost-updater.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+date=$(date)
+git pull origin master
+rm -r docs
+mkdir docs
+cd docs
+echo "nick-mc.github.io" > CNAME
+cd -
+ECTO1_SOURCE=http://localhost:2368 ECTO1_TARGET=https://nick-mc.github.io python3 ecto1.py
+cd docs
+docker cp nickmc_ghost_1:/var/lib/ghost/content/images/. content/images
+cd -
+grep -lR "srcset" docs/ | xargs sed -i 's/srcset/thisisbuggedatm/g'
+git add .
+git commit -m "$date"
+git config --global credential.helper store
+git push -u origin master
